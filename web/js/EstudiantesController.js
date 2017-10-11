44722fc2ab38401d990f253415abdca18883320f
memo1994 <guillermo.barrera@uptc.edu.co>
2017-10-11 13:26:32 -0500

    MVC
    taller 4 IS2

---
+++ web/js/EstudiantesController.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.controller('EstudiantesCtrl', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+        
+        //listar
+        $scope.lista = estudiantes;
+        $scope.datosFormulario = {};
+        $scope.panelEditar = false;
+        $scope.listaCarrera = carreras;
+        $scope.listaMunicipio = municipios;
+
+        //guardar
+        $scope.nuevo = function () {
+            $scope.panelEditar = true;
+            $scope.datosFormulario = {};
+        };
+
+        $scope.guardar = function () {
+            var index = $scope.lista.indexOf($scope.datosFormulario);
+            if (index === -1) {
+                $scope.datosFormulario.id=idEstudiantes++;
+                $scope.lista.push($scope.datosFormulario);
+            }
+            $scope.panelEditar = false;
+        };
+        $scope.cancelar = function () {
+            $scope.panelEditar = false;
+            $scope.datosFormulario = {};
+        };
+
+        //editar
+        $scope.editar = function (data) {
+            $scope.panelEditar = true;
+            $scope.datosFormulario = data;
+        };
+        //eliminar
+        $scope.eliminar = function (data) {
+            if (confirm('\xbfDesea elminar este registro?')) {
+                var index = $scope.lista.indexOf($scope.datosFormulario);
+                if (index > -1) {
+                    $scope.lista.splice(index, 1);
+                }
+            }
+        };
+      
+    }]);
+  
---
+++ web/js/Modelo.js
@@ -0,0 +1,45 @@
+var idFacultad=3;
+var facultades=[
+    {"id":1, "nombre":"Ingenieria"},
+    {"id":2, "nombre":"Licenciatura"}
+
+];
+var idCarrera=3;
+var carreras=[
+    {"id":1, "nombre":"Sistemas"},
+    {"id":2, "nombre":"Ambiental"}
+];
+
+var idMunicipio=4;
+var municipios=[
+    {"id":1, "nombre":"Tunja"},
+    {"id":2, "nombre":"Duitama"},
+    {"id":3, "nombre":"Sogamoso"},
+];
+
+var idEstudiantes=3;
+var estudiantes=[
+    {"id":1, "nombre":"memo", "apellido":"barrera", "codigo":"201623453", "documento":"12345","fechaNac":"08/09/1994"},
+    {"id":2, "nombre":"guillermo", "apellido":"orjuela", "codigo":"201789012", "documento":"6789","fechaNac":"24/01/1994"},
+];
+
+var idMateria=3;
+var materia=[
+    {"id":1, "nombre":"software 2", "creditos":4},
+    {"id":2, "nombre":"distribuidos", "creditos":3},
+];
+
+var idProfesor=2;
+var Profesor=[
+    {"id":1, "nombre":"jorge", "apellido":"otalora", "documento":"100000000"},
+];
+
+var idHorario=2;
+var horario=[
+    {"id":1, "dia_semana":3, "hora_inicio":10, "hora_fin":12},
+];
+
+var idMatricula=2;
+var matricula=[
+    {"id":1, "ano":3, "semestre":10, "estudinte":"ooo",}
+];

4fba38f128e5d54467721ed355c6273dae4db892
memo1994 <guillermo.barrera@uptc.edu.co>
2017-10-16 13:16:07 -0500

    MVC
    Taller 4 IS2

--- web/js/Modelo.js
+++ web/js/Modelo.js
@@ -1,13 +1,13 @@
 var idFacultad=3;
 var facultades=[
-    {"id":1, "nombre":"Ingenieria"},
+    {"id":1, "nombre":"Ingenieria Sistemas"},
     {"id":2, "nombre":"Licenciatura"}
 
 ];
 var idCarrera=3;
 var carreras=[
-    {"id":1, "nombre":"Sistemas"},
-    {"id":2, "nombre":"Ambiental"}
+    {"id":1, "nombre":"Sistemas", "facultad":{"id":1, "nombre":"Ingenieria Sistemas"}},
+    {"id":2, "nombre":"Ambiental","facultad":{"id":2, "nombre":"Licenciatura"}}
 ];
 
 var idMunicipio=4;
@@ -19,14 +19,14 @@ var municipios=[
 
 var idEstudiantes=3;
 var estudiantes=[
-    {"id":1, "nombre":"memo", "apellido":"barrera", "codigo":"201623453", "documento":"12345","fechaNac":"08/09/1994"},
-    {"id":2, "nombre":"guillermo", "apellido":"orjuela", "codigo":"201789012", "documento":"6789","fechaNac":"24/01/1994"},
+    {"id":1, "nombre":"memo", "apellido":"barrera", "codigo":"201623453", "documento":"12345","fechaNac":"31/12/1994","carrera":{"id":1, "nombre":"Sistemas"},"municipio":{"id":1, "nombre":"Tunja"}},
+    {"id":2, "nombre":"guillermo", "apellido":"orjuela", "codigo":"201789012", "documento":"6789","fechaNac":"24/12/1994","carrera":{"id":1, "nombre":"Ambiental"},"municipio":{"id":2, "nombre":"Duitama"}},
 ];
 
 var idMateria=3;
 var materia=[
-    {"id":1, "nombre":"software 2", "creditos":4},
-    {"id":2, "nombre":"distribuidos", "creditos":3},
+    {"id":1, "nombre":"software 2", "creditos":4,"carrera":{"id":1, "nombre":"Sistemas"},"profesor":{"id":1, "nombre":"jorge"}},
+    {"id":2, "nombre":"distribuidos", "creditos":3,"carrera":{"id":1, "nombre":"Ambiental"},"profesor":{"id":1, "nombre":"jorge"}},
 ];
 
 var idProfesor=2;
@@ -36,10 +36,10 @@ var Profesor=[
 
 var idHorario=2;
 var horario=[
-    {"id":1, "dia_semana":3, "hora_inicio":10, "hora_fin":12},
+    {"id":1, "dia_semana":3, "hora_inicio":10, "hora_fin":12,"materia":{"id":1, "nombre":"software 2"}},
 ];
 
 var idMatricula=2;
 var matricula=[
-    {"id":1, "ano":3, "semestre":10, "estudinte":"ooo",}
+    {"id":1, "ano":3, "semestre":10, "estudiante":{"id":1, "nombre":"memo"},"materia":{"id":1, "nombre":"software 2"}}
 ];
